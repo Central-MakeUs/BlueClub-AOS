4b6c6e0000e4736159b102c26f57bd00f3a30891
YuBeen-Park <park_ubin@naver.com>
2024-01-06 01:57:39 +0900

    [init] 확장 함수, 팀버 커스텀 구현

---
+++ app/src/main/java/org/blueclub/util/BlueClubDebugTree.kt
@@ -0,0 +1,8 @@
+package org.blueclub.util
+
+import timber.log.Timber
+
+class BlueClubDebugTree : Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement) =
+        "${element.fileName}:${element.lineNumber}#${element.methodName}"
+}
