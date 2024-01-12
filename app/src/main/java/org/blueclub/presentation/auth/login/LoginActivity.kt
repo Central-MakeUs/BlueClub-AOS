d70fc4525e7f3ef43111afafdbeeab5ef800ad7c
YuBeen-Park <park_ubin@naver.com>
2024-01-12 23:54:02 +0900

    [feat] #2 로그인뷰 UI 구현

---
+++ app/src/main/java/org/blueclub/presentation/auth/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package org.blueclub.presentation.auth.login
+
+import android.os.Bundle
+import org.blueclub.R
+import org.blueclub.databinding.ActivityLoginBinding
+import org.blueclub.presentation.base.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
