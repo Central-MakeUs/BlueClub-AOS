eeb8255b9c2d01eb853f80ab02205e71f474e305
YuBeen-Park <park_ubin@naver.com>
2024-01-16 22:00:18 +0900

    [feat] #2 회원가입 완료 뷰 구현

--- app/src/main/java/org/blueclub/presentation/auth/setting/fragment/TermsOfServiceBottomSheetDialog.kt
+++ app/src/main/java/org/blueclub/presentation/auth/setting/fragment/TermsOfServiceBottomSheetDialog.kt
@@ -1,10 +1,12 @@
 package org.blueclub.presentation.auth.setting.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import org.blueclub.R
 import org.blueclub.databinding.DialogTermsOfServiceBinding
+import org.blueclub.presentation.auth.setting.AuthSettingFinishActivity
 import org.blueclub.presentation.auth.setting.AuthSettingViewModel
 import org.blueclub.presentation.base.BindingBottomSheetDialogFragment
 
@@ -15,6 +17,19 @@ class TermsOfServiceBottomSheetDialog :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.btnTos.setOnClickListener {
+            moveToFinish()
+        }
+    }
+
+    private fun moveToFinish() {
+        val intent = Intent(requireActivity(), AuthSettingFinishActivity::class.java)
+        startActivity(intent)
+        dismiss()
     }
 
 }
