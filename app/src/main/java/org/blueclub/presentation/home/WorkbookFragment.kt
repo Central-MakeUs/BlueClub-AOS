1ce67e96398e58e2c6bc696984096e3e27600927
YuBeen-Park <park_ubin@naver.com>
2024-01-23 23:02:36 +0900

    [feat] #4 근무수첩, 마이페이지 Activity 파일 추가

---
+++ app/src/main/java/org/blueclub/presentation/home/WorkbookFragment.kt
@@ -0,0 +1,16 @@
+package org.blueclub.presentation.home
+
+import android.os.Bundle
+import android.view.View
+import org.blueclub.R
+import org.blueclub.databinding.FragmentWorkbookBinding
+import org.blueclub.presentation.base.BindingFragment
+
+class WorkbookFragment : BindingFragment<FragmentWorkbookBinding>(R.layout.fragment_workbook) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
