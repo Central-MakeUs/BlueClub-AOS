528870df678cf57ac239fa74b6e1ddfd4dd82697
YuBeen-Park <park_ubin@naver.com>
2024-01-23 23:02:06 +0900

    [feat] #4 홈 뷰 UI 구현

---
+++ app/src/main/java/org/blueclub/presentation/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package org.blueclub.presentation.home
+
+import android.os.Bundle
+import android.view.View
+import org.blueclub.R
+import org.blueclub.databinding.FragmentHomeBinding
+import org.blueclub.presentation.base.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
