98d001630fbb5eb5833a9dc40605d1581fab355a
YuBeen-Park <park_ubin@naver.com>
2024-01-24 15:54:14 +0900

    [feat] #6 근무수첩 뷰 구현

--- app/src/main/java/org/blueclub/presentation/home/MainActivity.kt
+++ app/src/main/java/org/blueclub/presentation/home/MainActivity.kt
@@ -1,6 +1,8 @@
 package org.blueclub.presentation.home
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import org.blueclub.R
 import org.blueclub.databinding.ActivityMainBinding
 import org.blueclub.presentation.base.BindingActivity
@@ -10,5 +12,15 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initLayout()
+    }
+
+    private fun initLayout(){
+        binding.bnvMain.itemIconTintList = null
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.fcv_main)?.findNavController()
+        navController?.let {
+            binding.bnvMain.setupWithNavController(it)
+        }
     }
 }
--- app/src/main/java/org/blueclub/presentation/home/WorkbookFragment.kt
+++ app/src/main/java/org/blueclub/presentation/home/WorkbookFragment.kt
@@ -2,15 +2,85 @@ package org.blueclub.presentation.home
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.children
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.daysOfWeek
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import com.kizitonwose.calendar.view.MonthDayBinder
+import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
+import com.kizitonwose.calendar.view.ViewContainer
 import org.blueclub.R
 import org.blueclub.databinding.FragmentWorkbookBinding
 import org.blueclub.presentation.base.BindingFragment
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.Locale
 
 class WorkbookFragment : BindingFragment<FragmentWorkbookBinding>(R.layout.fragment_workbook) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initLayout()
     }
 
+    private fun initLayout() {
+        binding.calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun bind(container: DayViewContainer, data: CalendarDay) {
+                if (data.position == DayPosition.MonthDate) {
+                    container.textView.text = data.date.dayOfMonth.toString()
+                }
+            }
+
+            // TODO outdate 일정의 경우 안 보이도록 수정하기
+            override fun create(view: View): DayViewContainer = DayViewContainer(view)
+
+        }
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
+        val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
+        val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
+        binding.calendarView.setup(startMonth, endMonth, firstDayOfWeek)
+        binding.calendarView.scrollToMonth(currentMonth)
+
+
+        // TODO 코드 리팩토링 필요
+        val titleContainer = requireActivity().findViewById<ViewGroup>(R.id.layout_title_container)
+        titleContainer.children
+            .map { it as TextView }
+            .forEachIndexed { index, textView ->
+                val dayOfWeek = daysOfWeek()[index]
+                val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                textView.text = title
+            }
+
+        binding.calendarView.monthHeaderBinder =
+            object : MonthHeaderFooterBinder<MonthViewContainer> {
+                override fun bind(container: MonthViewContainer, data: CalendarMonth) {
+                    if (container.titlesContainer.tag == null) {
+                        container.titlesContainer.tag = data.yearMonth
+                        container.titlesContainer.children.map { it as TextView }
+                            .forEachIndexed { index, textView ->
+                                val dayOfWeek = daysOfWeek()[index]
+                                val title =
+                                    dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                                textView.text = title
+                            }
+                    }
+                }
+
+                override fun create(view: View): MonthViewContainer = MonthViewContainer(view)
+
+            }
+    }
+
+}
+
+class MonthViewContainer(view: View) : ViewContainer(view) {
+    // Alternatively, you can add an ID to the container layout and use findViewById()
+    val titlesContainer = view as ViewGroup
 }
