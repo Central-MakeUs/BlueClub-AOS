44cbc593b4c69788cb05578d56dd95d64f582ce7
YuBeen-Park <park_ubin@naver.com>
2024-02-01 00:46:08 +0900

    [feat] #8 프로필 수정 갤러리에서 이미지 불러오는 로직 구현

--- app/src/main/java/org/blueclub/presentation/mypage/ProfileSettingActivity.kt
+++ app/src/main/java/org/blueclub/presentation/mypage/ProfileSettingActivity.kt
@@ -1,16 +1,66 @@
 package org.blueclub.presentation.mypage
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import org.blueclub.R
 import org.blueclub.databinding.ActivityProfileSettingBinding
 import org.blueclub.presentation.base.BindingActivity
 
-class ProfileSettingActivity:BindingActivity<ActivityProfileSettingBinding> (R.layout.activity_profile_setting){
-    private val viewModel : ProfileSettingViewModel by viewModels()
-    
+class ProfileSettingActivity :
+    BindingActivity<ActivityProfileSettingBinding>(R.layout.activity_profile_setting) {
+    private val viewModel: ProfileSettingViewModel by viewModels()
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initLayout()
     }
+
+    private fun initLayout() {
+        binding.ivProfile.clipToOutline = true
+        binding.ivCamera.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openGallery()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+            }
+        }
+    }
+
+
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted)
+                openGallery()
+        }
+
+    private val pickImageLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                data?.data?.let {
+                    imageUri = it
+                    binding.ivProfile.setImageURI(imageUri)
+                }
+            }
+        }
+
+    private fun openGallery() {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        pickImageLauncher.launch(gallery)
+    }
+
 }
