7f2333c9f52e62acbdd96b2577ec066ff02d6923
YuBeen-Park <park_ubin@naver.com>
2024-02-03 14:46:40 +0900

    [feat] network 관련 di 모듈 구현

---
+++ app/src/main/java/org/blueclub/di/NetworkModule.kt
@@ -0,0 +1,61 @@
+package org.blueclub.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.blueclub.BuildConfig
+import org.blueclub.BuildConfig.DEBUG
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @OptIn(ExperimentalSerializationApi::class)
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        isLenient = true
+        prettyPrint = true
+        explicitNulls = false
+        ignoreUnknownKeys = true
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().setLenient().create()
+
+    @ExperimentalSerializationApi
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, json: Json): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BC_BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        //.addConverterFactory(json.asConverterFactory(requireNotNull("application/json".toMediaTypeOrNull())))
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClientBuilder(
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            connectTimeout(10, TimeUnit.SECONDS)
+            writeTimeout(10, TimeUnit.SECONDS)
+            readTimeout(10, TimeUnit.SECONDS)
+            if (DEBUG) addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+        }.build()
+}
