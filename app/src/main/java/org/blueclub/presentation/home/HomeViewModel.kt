226e69518eca3813fbfc8f55ccb67c607bc9a2ea
YuBeen-Park <park_ubin@naver.com>
2024-02-05 21:52:04 +0900

    [mod] 홈 뷰 UI 데이터 연결

--- app/src/main/java/org/blueclub/presentation/home/HomeViewModel.kt
+++ app/src/main/java/org/blueclub/presentation/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package org.blueclub.presentation.home
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.blueclub.data.datasource.BCDataSource
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +13,5 @@ class HomeViewModel @Inject constructor(
 
     val nickname = localStorage.nickname
     val job = localStorage.job
+    val month = LocalDateTime.now().month.value
 }
