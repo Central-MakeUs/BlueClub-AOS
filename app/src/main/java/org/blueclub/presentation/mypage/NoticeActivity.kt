c92b679cbd4131f6807fee646ad1faf36e2fa804
YuBeen-Park <park_ubin@naver.com>
2024-02-05 22:04:22 +0900

    [chore] 클래스 네이밍 변경

--- app/src/main/java/org/blueclub/presentation/mypage/NoticeActivity.kt
+++ app/src/main/java/org/blueclub/presentation/mypage/NoticeActivity.kt
@@ -2,11 +2,11 @@ package org.blueclub.presentation.mypage
 
 import android.os.Bundle
 import org.blueclub.R
-import org.blueclub.databinding.ActivityNotificationBinding
+import org.blueclub.databinding.ActivityNoticeBinding
 import org.blueclub.presentation.base.BindingActivity
 
-class NotificationActivity :
-    BindingActivity<ActivityNotificationBinding>(R.layout.activity_notification) {
+class NoticeActivity :
+    BindingActivity<ActivityNoticeBinding>(R.layout.activity_notice) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
