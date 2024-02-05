e6591ef1e054004a95afeb3c4b1589d2078ffbcf
YuBeen-Park <park_ubin@naver.com>
2024-02-05 21:55:27 +0900

    [feat] 마이페이지 뷰모델 추가

--- app/src/main/java/org/blueclub/presentation/mypage/MyPageFragment.kt
+++ app/src/main/java/org/blueclub/presentation/mypage/MyPageFragment.kt
@@ -3,11 +3,14 @@ package org.blueclub.presentation.mypage
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import org.blueclub.R
 import org.blueclub.databinding.FragmentMyPageBinding
 import org.blueclub.presentation.base.BindingFragment
+import org.blueclub.presentation.home.HomeViewModel
 
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private val viewModel: HomeViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
